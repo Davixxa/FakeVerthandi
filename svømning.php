2beec742212824c77ef35afc9a3d50845fc9b78a
Davixxa <w890ik660i@hotmail.com>
2019-03-20 12:15:42 +0100

    Started work on control panels.

--- svømning.php
+++ svømning.php
@@ -1,21 +1,26 @@
 <?php require("header.php") ?>
+<div class="container">
+  <br>
 
-<div class="jumbotron">
-    <h1 class="display-4">Svømning</h1>
-    <p class="lead">Vi har forskellige niveauer indenfor svømning</p>
-    <hr class="my-4">
-    <p>Vi har alt fra nybegyndere, lidt øvet, øvet, mini talent og talent.</p>
-    <a class="btn btn-primary btn-lg" href="#" role="button">Læs mere</a>
-  </div>
-  <div class="list-group">
-    <button type="button" class="list-group-item list-group-item-action active">
-      Hold
-    </button>
-    <button type="button" class="list-group-item list-group-item-action">Nybegyndere</button>
-    <button type="button" class="list-group-item list-group-item-action">Lidt øvet</button>
-    <button type="button" class="list-group-item list-group-item-action">Øvet</button>
-    <button type="button" class="list-group-item list-group-item-action">Mini talent</button>
-    <button type="button" class="list-group-item list-group-item-action">Talent</button>
-  </div>
+  <div class="jumbotron">
+      <h1 class="display-4">Svømning</h1>
+      <p class="lead">Vi har forskellige niveauer indenfor svømning</p>
+      <hr class="my-4">
+      <p>Vi har alt fra nybegyndere, lidt øvet, øvet, mini talent og talent.</p>
+      <a class="btn btn-primary btn-lg" href="#" role="button">Læs mere</a>
+    </div>
+    <div class="list-group">
+      <button type="button" class="list-group-item list-group-item-action active">
+        Hold
+      </button>
+      <button type="button" class="list-group-item list-group-item-action">Nybegyndere</button>
+      <button type="button" class="list-group-item list-group-item-action">Lidt øvet</button>
+      <button type="button" class="list-group-item list-group-item-action">Øvet</button>
+      <button type="button" class="list-group-item list-group-item-action">Mini talent</button>
+      <button type="button" class="list-group-item list-group-item-action">Talent</button>
+    </div>
+
+
+</div>
 
   <?php require("footer.php") ?>
---
+++ userCP.php
@@ -0,0 +1,12 @@
+<?php require("header.php"); 
+$pagetitle = "Min Idrætsforening | Brugerkontrolpanel";
+?>
+
+<div class="container"><br>
+    <div class="jumbotron">
+    <h1 class="display-4">Brugerkontrolpanel</h1>
+    <p class="lead">Her kan du tilmelde og framelde dig forskellige hold.</p>
+  </div></div>
+
+
+<?php require("footer.php"); ?>
