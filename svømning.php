1fd1e6573c145dbdc4b9981c4489564dd08fecf4
delcacs <47742046+delcacs@users.noreply.github.com>
2019-03-20 12:34:10 +0100

    fix mathias

--- svømning.php
+++ svømning.php
@@ -17,8 +17,15 @@
       <div id="nybegynderCollapse" class="collapse">
 			<div class="list-group-item list-group-item-action">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt placeat voluptatem veniam, suscipit rem inventore officiis repellendus. Repellendus deserunt officiis itaque at soluta, dolores, excepturi, voluptatem harum tempore dolorem in!</div>
 		</div>
-      <button type="button" class="list-group-item list-group-item-action">Lidt øvet</button>
-      <button type="button" class="list-group-item list-group-item-action">Øvet</button>
+    <button type="button" class="list-group-item list-group-item-action" data-toggle="collapse" data-target="#lidtOevetCollapse" aria-expanded="false" aria-controls="lidtOevetCollapse">Lidt &oslash;vet</button>
+      <div id="lidtOevetCollapse" class="collapse">
+			<div class="list-group-item list-group-item-action">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt placeat voluptatem veniam, suscipit rem inventore officiis repellendus. Repellendus deserunt officiis itaque at soluta, dolores, excepturi, voluptatem harum tempore dolorem in!</div>
+		</div>
+    </div>
+    <button type="button" class="list-group-item list-group-item-action" data-toggle="collapse" data-target="#OevetCollapse" aria-expanded="false" aria-controls="OevetCollapse">&Oslash;vet</button>
+      <div id="OevetCollapse" class="collapse">
+			<div class="list-group-item list-group-item-action">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt placeat voluptatem veniam, suscipit rem inventore officiis repellendus. Repellendus deserunt officiis itaque at soluta, dolores, excepturi, voluptatem harum tempore dolorem in!</div>
+		</div>
       <button type="button" class="list-group-item list-group-item-action">Mini talent</button>
       <button type="button" class="list-group-item list-group-item-action">Talent</button>
     </div>
