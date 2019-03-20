e8bf851465a7d3ff0f3d5c9897e3f8dc87c5f5e3
Mathias <mathiasmh@live.dk>
2019-03-20 13:05:01 +0100

    Hej :)

--- fitness.php
+++ fitness.php
@@ -7,7 +7,6 @@
       <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, numquam.</p>
       <hr class="my-4">
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime beatae numquam fugiat nihil illo sequi magnam culpa distinctio, cupiditate, exercitationem commodi error quae unde quam asperiores nobis nemo aperiam suscipit. Eius nam nihil possimus labore ipsa. Facere harum natus exercitationem, aliquam provident aut hic amet facilis laborum, libero molestias recusandae!</p>
-      <a class="btn btn-primary btn-lg" href="#" role="button">Læs mere</a>
     </div>
    
 
