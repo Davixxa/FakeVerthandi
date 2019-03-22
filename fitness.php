ed5355936eb645d04d996c5831771057bcf7e2f7
Davixxa <w890ik660i@hotmail.com>
2019-03-22 17:03:42 +0100

    Fix?

--- fitness.php
+++ fitness.php
@@ -8,6 +8,7 @@
       <hr class="my-4">
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime beatae numquam fugiat nihil illo sequi magnam culpa distinctio, cupiditate, exercitationem commodi error quae unde quam asperiores nobis nemo aperiam suscipit. Eius nam nihil possimus labore ipsa. Facere harum natus exercitationem, aliquam provident aut hic amet facilis laborum, libero molestias recusandae!</p>
     </div>
-   
+
+</div>
 
   <?php require("footer.php") ?>
